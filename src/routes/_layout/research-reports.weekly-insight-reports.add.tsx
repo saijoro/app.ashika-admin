d00f1aa56a21814ea5689add7a61024346e99417
satygopal <satyagopalajiyanna.c@orotron.com>
2024-10-08 20:10:16 +0530

    GO: created add Screen

---
+++ src/routes/_layout/research-reports.weekly-insight-reports.add.tsx
@@ -0,0 +1,13 @@
+import CombineAdd from "@/components/core/AddReports/CombineAdd";
+import { CreateReportProvider } from "@/components/core/AddReports/CreateReportContext";
+import { createFileRoute } from "@tanstack/react-router";
+
+export const Route = createFileRoute(
+  "/_layout/research-reports/weekly-insight-reports/add"
+)({
+  component: () => (
+    // <CreateReportProvider>
+    <CombineAdd />
+    // </CreateReportProvider>
+  ),
+});
