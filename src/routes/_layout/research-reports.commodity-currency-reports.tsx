324854d2caff2bee63a702149fcc2c508b77b878
satygopal <satyagopalajiyanna.c@orotron.com>
2024-10-09 16:23:03 +0530

    GO: Side Menu active classes

--- src/routes/_layout/research-reports.commodity-currency-reports.tsx
+++ src/routes/_layout/research-reports.commodity-currency-reports.tsx
@@ -1,14 +1,16 @@
 import Reports from "@/components/Reports";
-import { createFileRoute } from "@tanstack/react-router";
+import { Outlet, createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute(
   "/_layout/research-reports/commodity-currency-reports"
 )({
   component: () => (
-    <Reports
-      reportGroup={"research-reports"}
-      reportType={"daily-insights-reports"}
-      categoryType={""}
-    />
+    <>
+      <Reports
+        reportGroup={"research-reports"}
+        reportType={"daily-insights-reports"}
+        categoryType={""}
+      />
+    </>
   ),
 });
