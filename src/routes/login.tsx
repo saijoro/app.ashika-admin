3ce8d910318925b023ce51c48e2123e235fd19fa
manisha.oro <manisha.d@orotron.com>
2024-10-11 07:20:54 +0530

    Spot fix build errors

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,10 +1,5 @@
-import { createFileRoute, useNavigate } from "@tanstack/react-router";
-import LogoPath from "@/assets/logo.png";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { LogIn } from "lucide-react";
-import LoginComponent from "@/components/auth/LogIn";
+import LoginComponent from "@/components/auth/Login";
+import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/login")({
   component: () => (
